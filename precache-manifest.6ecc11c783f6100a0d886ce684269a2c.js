c6ea275135c0b726ccbb89a4e53e8031403e60bb
YerSar87 <esargsyan1987@gmail.com>
2020-04-20 18:19:36 +0400

    Updates

---
+++ precache-manifest.6ecc11c783f6100a0d886ce684269a2c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436c85db7d578bb1413efa2834610761",
+    "url": "/ArmPoetry/index.html"
+  },
+  {
+    "revision": "490862458d1258f56a1e",
+    "url": "/ArmPoetry/static/css/3.89402874.chunk.css"
+  },
+  {
+    "revision": "85801f09dab6ca217dd4",
+    "url": "/ArmPoetry/static/css/4.bcb732fc.chunk.css"
+  },
+  {
+    "revision": "4bf55b7f39e188732be0",
+    "url": "/ArmPoetry/static/css/main.7f5f7ec0.chunk.css"
+  },
+  {
+    "revision": "65bdeab415a4c03918e6",
+    "url": "/ArmPoetry/static/js/2.5490dc91.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/ArmPoetry/static/js/2.5490dc91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490862458d1258f56a1e",
+    "url": "/ArmPoetry/static/js/3.2063b77b.chunk.js"
+  },
+  {
+    "revision": "85801f09dab6ca217dd4",
+    "url": "/ArmPoetry/static/js/4.e4ee970e.chunk.js"
+  },
+  {
+    "revision": "4bf55b7f39e188732be0",
+    "url": "/ArmPoetry/static/js/main.4ced2d0b.chunk.js"
+  },
+  {
+    "revision": "e81565bdef67a927dabb",
+    "url": "/ArmPoetry/static/js/runtime-main.c54aa25e.js"
+  },
+  {
+    "revision": "ef80e3ab8397f3f8fbf111c7d4640c29",
+    "url": "/ArmPoetry/static/media/charentc.ef80e3ab.jpg"
+  },
+  {
+    "revision": "e1ab209c10cafc68dfaf1af44c1b38f5",
+    "url": "/ArmPoetry/static/media/default.e1ab209c.png"
+  }
+]);
